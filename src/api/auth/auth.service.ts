463bc1eccc11481b8ca51299c50fa25432e582e1
Makar <makardovgopolji@gmail.com>
2024-06-29 16:41:59 +0200

    refacroting

--- src/api/auth/auth.service.ts
+++ src/api/auth/auth.service.ts
@@ -1,23 +1,36 @@
 import { Injectable } from '@nestjs/common';
 import { JwtService } from '@nestjs/jwt';
 import * as bcrypt from 'bcrypt';
+import { User } from '../user/model/user.model';
 
 @Injectable()
 export class AuthService {
 	constructor(private jwtService: JwtService) { }
 
 	async hashPassword(password: string): Promise<string> {
-		const saltOrRounds = 10;
-		return bcrypt.hash(password, saltOrRounds);
+		try {
+			const saltOrRounds = 10;
+			return bcrypt.hash(password, saltOrRounds);
+		} catch (error) {
+			throw new Error(error)
+		}
 	}
 
-	async decodePassword(password: string, hashPassword: string) {
-		const isMatch = await bcrypt.compare(password, hashPassword);
-		return isMatch
+	async decodePassword(password: string, hashPassword: string): Promise<boolean> {
+		try {
+			const isMatch = await bcrypt.compare(password, hashPassword);
+			return isMatch
+		} catch (error) {
+			throw new Error(error)
+		}
 	}
 
-	generateToken(user: any): Promise<string> {
-		const payload = { user };
-		return this.jwtService.signAsync(payload)
+	generateToken(user: User): Promise<string> {
+		try {
+			const payload = { user };
+			return this.jwtService.signAsync(payload)
+		} catch (error) {
+			throw new Error(error)
+		}
 	}
 }
